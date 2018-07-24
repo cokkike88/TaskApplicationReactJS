d41c7d0284ae14d8f41c8d8e3b065a7c0df29437
Oscar <olima@pidelorapido.com>
2018-07-24 16:32:14 -0600

    Muestra las tareas falta crear el formulario para agragar las tareas, el video 3

--- src/App.js
+++ src/App.js
@@ -1,19 +1,58 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Navigation from './components/navigation';
+
+import { todos } from './Data/todos.json';
 
 class App extends Component {
+
+  title;
+  number;
+
+  constructor(){
+    super();
+    this.state = {
+      title: "Tasks",
+      todos
+    }
+
+    this.title = this.state.title; 
+    this.number = this.state.todos.length;
+  }
+
   render() {
+
+    const todos = this.state.todos.map((todo, i) => {
+      return (
+        <div className="col-md-4">
+          <div className="card mt-4">
+            <div className="card-header">
+              <h3>{todo.title}</h3>
+              <span className="badge badge-pill badge-danger ml-2">
+                {todo.priority}
+              </span>
+            </div>
+            <div className="card-body">
+              <p>{todo.description}</p>
+              <p><mark>{todo.responsible}</mark></p>
+            </div>
+          </div>
+        </div>
+      )
+    });
+
     return (
-      <div className="App">
-        <header className="App-header">
+      <div className="App">    
+          <Navigation title = {this.title} number= {this.number} />
           <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+          <div className="container">
+            <div className="row mt-4">
+              { todos }
+            </div>
+          </div>
       </div>
+      
     );
   }
 }
