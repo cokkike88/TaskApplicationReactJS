4b034d71345b3bb1d4b9c70690756b932f6987ad
Oscar <olima@pidelorapido.com>
2018-08-10 16:35:20 -0600

    form

--- src/components/todoForm.js
+++ src/components/todoForm.js
@@ -3,6 +3,24 @@ import React, { Component} from 'react';
 
 class TodoForm extends Component{
 
+    
 
+    render(){
+        return (
+            <div className="card">
+                <form className="card-body">
+                    <div className="form-group">
+                        <input type="text" name="title" onChange="" className="form-control" placeholder="title" />
+                    </div>
+                    <div className="form-group">
+                        <input type="text" name="responsible" className="form-control" placeholder="responsible" />
+                    </div>
+                    <div className="form-group">
+                        <input type="text" name="description" className="form-control" placeholder="description" />
+                    </div>
+                </form>
+            </div>
+        )
+    }
     
 }
